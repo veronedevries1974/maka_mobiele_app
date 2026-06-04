90a320e00e1a9bd05a338ea9ccc17c0e7265be58
Verone de Vries <veronedevries@gmail.com>
2026-06-04 12:18:02 +0200

    conceptversie

--- app/app-routing.module.ts
+++ app/app-routing.module.ts
@@ -2,15 +2,23 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { ParentComponent } from './parent/parent.component';
 import { ChildComponent } from './child/child.component';
+import { Child2Component } from './child2/child2.component'; // GECORRIGEERD: Deze import ontbrak!
 import { Child2aComponent } from './child2a/child2a.component';
 import { Child2bComponent } from './child2b/child2b.component';
 
 const routes: Routes = [
-  { path: '', component: ParentComponent },
-  { path: 'prijslijst', component: ChildComponent },
-  { path: 'activiteiten/wandelen', component: Child2aComponent },
-  { path: 'activiteiten/kanoen', component: Child2bComponent },
-  { path: '**', redirectTo: '' } // Catch-all: stuurt onbekende routes terug naar home
+  { path: '', component: ParentComponent }, // home
+  { path: 'child', component: ChildComponent }, // prijslijst
+  { 
+    path: 'child2', 
+    component: Child2Component, // activiteiten
+    children: [
+      /* Als iemand naar /child2 gaat, sturen we ze direct door naar de wandelpagina */
+      { path: '', redirectTo: 'wandelen', pathMatch: 'full' }, 
+      { path: 'wandelen', component: Child2aComponent }, // wandelen in activiteiten
+      { path: 'kanoen', component: Child2bComponent }   // kanoen in activiteiten
+    ]
+  }
 ];
 
 @NgModule({
